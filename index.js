82cac14a2e60fa8038c94a4951d793340ef5c3a0
jonschlinkert <dev@sellside.com>
2014-06-19 06:55:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+/*!
+ * plugins <https://github.com/jonschlinkert/plugins>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+
+/**
+ * Initialize `Plugins`
+ *
+ * ```js
+ * var plugins = new Plugins();
+ * ```
+ * @constructor
+ * @api public
+ */
+
+function Plugins() {
+  this.plugins = [];
+};
+
+
+/**
+ * ## .use
+ *
+ * Add a plugin `fn` to the plugins.
+ *
+ * ```js
+ * plugins
+ *   .use(foo({}))
+ *   .use(bar({}))
+ *   .use(baz({}))
+ * ```
+ *
+ * @param {Function} `fn` Plugin function to add to the `plugins` stack.
+ * @param {Options} `options` Options to pass to `fn`.
+ * @return {this} for chaining.
+ * @api public
+ */
+
+Plugins.prototype.use = function (fn) {
+  this.plugins.push(fn.bind(this));
+  return this;
+};
+
+
+/**
+ * ## .run
+ *
+ * Run `str` against each function in the plugins.
+ *
+ * ```js
+ * plugins.run(str, opts)
+ * ```
+ *
+ * @param {String} `str` The string to transform.
+ * @param {String} `opts` Options to pass to all plugins in the stack.
+ * @return {String} transformed string.
+ * @api public
+ */
+
+Plugins.prototype.run = function (str, opts) {
+  return this.plugins.reduce(function(content, fn) {
+    if (typeof fn(content) !== 'function') {
+      return fn(content, opts);
+    }
+    return fn(opts)(content);
+  }, str);
+};
+
+
+
+// Export `Plugins`
+module.exports = Plugins;
---
+++ test/test.js
@@ -0,0 +1,125 @@
+/*!
+ * plugins <https://github.com/jonschlinkert/plugins>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+var file = require('fs-utils');
+var expect = require('chai').expect;
+var Plugins = require('../');
+
+function fixture(filename) {
+  return file.readFileSync('test/fixtures/' + filename);
+}
+
+function actual(filename, content) {
+  return file.writeFileSync('test/actual/' + filename, content);
+}
+
+
+describe('plugins.run():', function () {
+  describe('when a string is passed to plugins.run():', function () {
+    it('should run the string through each plugin in the stack.', function () {
+      var plugins = new Plugins();
+
+      plugins
+        .use(function (options) {
+          return function(str) {
+            var re = /[\r\n]/;
+            return str.split(re).map(function (line, i) {
+              return '\naaa' + line + 'bbb';
+            }).join('');
+          }
+        })
+
+        .use(function (options) {
+          return function(str) {
+            return str + 'ccc';
+          }
+        })
+
+        .use(function (options) {
+          return function(str) {
+            return str + 'ddd';
+          }
+        });
+
+      var str = plugins.run(fixture('LICENSE-MIT'));
+      var test = /cccddd$/.test(str);
+      expect(test).to.equal(true);
+    });
+  });
+});
+
+
+describe('when a plugin is passed:', function () {
+  it('should run the function and return the result.', function () {
+    var plugins = new Plugins();
+
+    var src = function (options) {
+      return function(filepath) {
+        return fixture(filepath);
+      }
+    };
+
+    plugins.use(src());
+
+    var str = plugins.run('LICENSE-MIT');
+    // Test the date in the license
+    expect(new RegExp((new Date).getUTCFullYear()).test(str)).to.equal(true);
+  });
+});
+
+describe('when a plugin is passed with options:', function () {
+  it('should run the function and return the result.', function () {
+    var plugins = new Plugins();
+
+    var src = function (options) {
+      return function(filepath) {
+        var year = new RegExp((new Date).getUTCFullYear());
+        var str = fixture(filepath);
+        return str.replace(year, options.year || '');
+      }
+    };
+
+    plugins.use(src({year: 'Stardate 3000'}))
+
+    var str = plugins.run('LICENSE-MIT');
+    // Test the custom string in the license
+    expect(/Stardate/.test(str)).to.equal(true);
+  });
+});
+
+describe('when a plugin is passed a file path:', function () {
+  it('should read the file with the first plugin, then run the string through the rest of the stack.', function () {
+    var plugins = new Plugins();
+
+    var src = function (filepath, options) {
+      return function(str, globals) {
+        return options.prepend + file.readFileSync(filepath);
+      };
+    };
+
+    var append = function (footer) {
+      return function(str) {
+        return str + footer;
+      };
+    };
+
+    var dest = function (filepath) {
+      return function(str) {
+        return actual(filepath, str);
+      };
+    };
+
+    plugins
+      .use(src('LICENSE-MIT', {prepend: 'banner'}), {local: 'options'})
+      .use(append('footer.', {footer: 'opts'}), {a: 'b'})
+      .use(dest('footer.md'));
+
+    plugins.run({global: 'options'}, {c: 'd'});
+    expect(file.exists('test/actual/footer.md')).to.equal(true);
+    // file.delete('test/actual/footer.md');
+  });
+});
